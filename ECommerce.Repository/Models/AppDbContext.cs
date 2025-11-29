dd73ded055426f5eb29b5e4e7cb00fccc2145229
javedahmedone <javedahmedone@gmail.com>
2025-11-29 19:12:36 +0530

    update

--- ECommerce.Repository/Models/AppDbContext.cs
+++ ECommerce.Repository/Models/AppDbContext.cs
@@ -27,9 +27,6 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<UserRole> UserRoles { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=db.zmoyjzbgyujeyxgvdzku.supabase.co;Database=postgres;Username=postgres;Password=V2ludGVyQDIwMjUK@;SSL Mode=Require;Trust Server Certificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
